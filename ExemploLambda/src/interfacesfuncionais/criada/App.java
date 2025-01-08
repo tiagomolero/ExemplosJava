f0864d8a8bb460b50ae6f085a6b83da658dfa60b
Tiago Molero <tiagomolero@gmail.com>
2025-01-08 20:09:22 -0300

    Exemplo BiFunction

--- ExemploLambda/src/interfacesfuncionais/criada/App.java
+++ ExemploLambda/src/interfacesfuncionais/criada/App.java
@@ -1,5 +1,7 @@
 package interfacesfuncionais.criada;
 
+import java.util.function.BiFunction;
+
 public class App {
 
     public static void main(String[] args) {
@@ -11,6 +13,15 @@ public class App {
         Operacao subtracao = (a,b) -> a - b;
         System.out.println("O resultado da subtração é: " + subtracao.calcular(5,2));
 
+        Long returnLong = calcular(1L, 5L,(Long b1, Long b2) -> {
+          return b1 + b2;
+        });
+
+        System.out.println(returnLong);
+    }
+
+    private static Long calcular(Long n1, Long n2, BiFunction<Long, Long, Long> biFunction){
+        return biFunction.apply(n1,n2);
     }
 
 }
