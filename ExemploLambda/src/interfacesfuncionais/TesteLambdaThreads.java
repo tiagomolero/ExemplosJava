75ba7ccd572b23169109fd166e2c30d550932a7c
Tiago Molero <tiagomolero@gmail.com>
2025-01-09 17:31:34 -0300

    Adicionando Métodos em expressões lambdas

--- ExemploLambda/src/interfacesfuncionais/TesteLambdaThreads.java
+++ ExemploLambda/src/interfacesfuncionais/TesteLambdaThreads.java
@@ -8,18 +8,26 @@ public class TesteLambdaThreads {
         Runnable r = new Runnable() {
             @Override
             public void run() {
-                System.out.println("Rodando Runnable Normal");
+                //System.out.println("Rodando Runnable Normal");
+                imprimit("Rodando Runnable Normal");
             }
         };
         new Thread(r).start();
 
         //Runnable com Lambda
-        Runnable rLambda = () -> System.out.println("Rodando Runnale com Lambda");
+        //Runnable rLambda = () -> System.out.println("Rodando Runnale com Lambda");
+        Runnable rLambda = () -> imprimit("Rodando Runnale com Lambda");
         new Thread(rLambda).start();
 
         //Thread com Lambda e com Runnable
-        new Thread(() -> System.out.println("Rodando Thread Runnable com Lambda")).start();
+        //new Thread(() -> System.out.println("Rodando Thread Runnable com Lambda")).start();
+        new Thread(() -> imprimit("Rodando Thread Runnable com Lambda")).start();
 
+    }
+
+    // Criando Método para inserir na expressão Lambda
+    public static <T> void imprimit(T value){
+        System.out.println(value);
     }
 
 }
